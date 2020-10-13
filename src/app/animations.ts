cb075c879462140d3138d573565daeb43523602c
berezkin88 <berezkin05@gmail.com>
2020-10-14 00:11:47 +0300

    playjoy with animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,18 +1,41 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, animation, keyframes, state, style, transition, trigger, useAnimation } from '@angular/animations';
+
+export let bounceOutLeftAnimation = animation(
+  animate('0.5s ease-in', keyframes([
+    style({
+      offset: .2,
+      opacity: 1,
+      transform: 'translateX(20px)'
+    }),
+    style({
+      offset: 1,
+      opacity: 0,
+      transform: 'translateX(-100%)'
+    })
+  ]))
+);
+
+export let fadeInAnimation = animation([
+  style({ opacity: 0 }),
+  animate('{{ duration }} {{ easing }}')
+], {
+  params: {
+    duration: '2s',
+    easing: 'ease-out'
+  }
+});
 
 export let fade = trigger('fade', [
-  state('void', style({ opacity: 0 })),
-  transition(':enter, :leave', [
-    animate(2000)
+  transition(':enter', useAnimation(fadeInAnimation)),
+  transition(':leave', [
+    animate(2000, style({ opacity: 0 }))
   ])
 ]);
 
-export let slide = trigger('fade', [
+export let slide = trigger('slide', [
   state('void', style({ transform: 'translateX(-20px)' })),
   transition(':enter', [
     animate(1000)
   ]),
-  transition(':leave', [
-    animate(200, style({ transform: 'translateX(-400px)' }))
-  ])
+  transition(':leave', useAnimation(bounceOutLeftAnimation))
 ]);
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,13 +1,40 @@
-import { fade, slide } from './../animations';
+import { fade, slide, bounceOutLeftAnimation, fadeInAnimation } from './../animations';
 
 import { Component } from '@angular/core';
+import { animate, animateChild, group, query, stagger, style, transition, trigger, useAnimation } from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
-    slide
+    trigger('todosAnimation', [
+      transition(':enter', [
+        group([
+          query('h1', [
+            style({ transform: 'translateY(-20px)' }),
+            animate(1000)
+          ]),
+          query('@todoAnimation',
+            stagger(200, animateChild())
+          )
+        ])
+      ])
+    ]),
+    trigger('todoAnimation', [
+      transition(':enter', [
+        useAnimation(fadeInAnimation, {
+          params: {
+            duration: '500ms'
+          }
+        })
+      ]),
+      transition(':leave', [
+        style({ backgroundColor: 'red' }),
+        animate(1000),
+        useAnimation(bounceOutLeftAnimation)
+      ])
+    ])
   ]
 })
 export class TodosComponent {
@@ -25,4 +52,7 @@ export class TodosComponent {
     let index = this.items.indexOf(item);
     this.items.splice(index, 1);
   }
+
+  animationStarted($event) { console.log($event) }
+  animationDone($event) { console.log($event) }
 }
